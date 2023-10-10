7c1019fe87aaccd930eddced22decd42c48e19d5
rbsingh001 <robinhooda25@gmail.com>
2023-10-10 14:26:23 +0530

    page ok on refresh

--- app.js
+++ app.js
@@ -16,7 +16,55 @@ function addUser() {
         .post("https://crudcrud.com/api/482ecc20abe7477a88e458fd5ca81571/users",
             user
         )
-        .then(response => console.log(response))
+        .then(response => {
+            console.log(response)
+            ShowNewUser(response.data)
+        })
         .catch(error => console.log(error))
 
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/482ecc20abe7477a88e458fd5ca81571/users")
+        .then((response) =>{
+            console.log(response)
+            for(let i=0; i<response.data.length; i++){
+                ShowNewUser(response.data[i]);
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+
+function ShowNewUser(u){
+    document.getElementById("name").value="";
+
+    document.getElementById("email").value="";
+
+    document.getElementById("phn").value="";
+
+    let ul = document.getElementById("ul")
+
+   
+
+        let li = document.createElement("li");
+        let t = document.createTextNode( u.name+" "+ u.email +" "+ u.phone);
+        console.log(t);
+
+        li.appendChild(t);
+
+        var deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+
+        var editbtn = document.createElement('button');
+        editbtn.innerText = 'Edit';
+
+        li.appendChild(deleteButton);
+        li.appendChild(editbtn);
+        ul.appendChild(li);
+    
+
+    
 }
