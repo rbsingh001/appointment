10ca537cd7331d77d7a317e7d37bb3333b2f6fb0
rbsingh001 <robinhooda25@gmail.com>
2023-10-11 18:38:37 +0530

    Edit button works with reload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+var e = 0;
+var eid;
 function addUser() {
 
     let name = document.getElementById("name").value;
@@ -12,20 +14,35 @@ function addUser() {
         email,
         phone
     }
-    axios
-        .post("https://crudcrud.com/api/482ecc20abe7477a88e458fd5ca81571/users",
+
+    if (e = 0) {
+        axios
+            .post("https://crudcrud.com/api/e01dc420f2a94f3580b67c4c5f08b176/users",
+                user
+            )
+            .then(response => {
+                console.log(response)
+                ShowNewUser(response.data)
+            })
+            .catch(error => console.log(error))
+    }
+    if (e = 1) {
+        e = 0;
+        axios.put(`https://crudcrud.com/api/e01dc420f2a94f3580b67c4c5f08b176/users/${eid}`,
             user
         )
-        .then(response => {
-            console.log(response)
-            ShowNewUser(response.data)
-        })
-        .catch(error => console.log(error))
+            .then((response) => {
+                console.log(response)
+                location.reload();
+            })
+            .catch(error => console.log(error))
+    }
+
 
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/482ecc20abe7477a88e458fd5ca81571/users")
+    axios.get("https://crudcrud.com/api/e01dc420f2a94f3580b67c4c5f08b176/users")
         .then((response) => {
             console.log(response)
             for (let i = 0; i < response.data.length; i++) {
@@ -47,42 +64,57 @@ function ShowNewUser(u) {
 
     let ul = document.getElementById("ul")
 
-
-
     let li = document.createElement("li");
     let t = document.createTextNode(u.name + " " + u.email + " " + u.phone);
-    console.log(t);
 
     li.appendChild(t);
 
     var deleteButton = document.createElement('button');
     deleteButton.innerText = 'Delete';
     let list = document.getElementById("ul")
-    deleteButton.addEventListener("click", function(){
-        // deleteUser(u);
-        axios.delete(`https://crudcrud.com/api/482ecc20abe7477a88e458fd5ca81571/users/${u._id}`)
-        .then((response) => {
-            li.remove();
-        })
-        .catch((error) => console.log(error));
+    deleteButton.addEventListener("click", function () {
+        deleteUser(u._id);
 
     })
 
     var editbtn = document.createElement('button');
     editbtn.innerText = 'Edit';
+    editbtn.addEventListener("click", function () {
+        editUser(u);
+    })
 
     li.appendChild(deleteButton);
     li.appendChild(editbtn);
+
+
+    li.setAttribute("id", `${u._id}`);
     ul.appendChild(li);
 
 
 
 }
 
-// function deleteUser(x){
-//     axios.delete(`https://crudcrud.com/api/482ecc20abe7477a88e458fd5ca81571/users/${x._id}`)
-//         .then((response) => {
-//             li.remove();
-//         })
-//         .catch((error) => console.log(error));
-// }
+function deleteUser(x) {
+    axios.delete(`https://crudcrud.com/api/e01dc420f2a94f3580b67c4c5f08b176/users/${x}`)
+        .then((response) => {
+            var liToRemove = document.getElementById(`${x}`);
+            if (liToRemove) {
+                liToRemove.remove();
+
+            }
+        })
+        .catch((error) => console.log(error));
+}
+function editUser(z) {
+
+    document.getElementById("name").value = z.name;
+
+    document.getElementById("email").value = z.email;
+
+    document.getElementById("phn").value = z.phone;
+
+    e = 1;
+    eid = z._id;
+    console.log(eid);
+
+}
